6fa7aaeaa121c1df665c2856c00181235edd49dc
maximiliankoch <tschaka1904@gmail.com>
2015-10-13 10:48:53 +0100

    better mvn command

--- run-collector.sh
+++ run-collector.sh
@@ -1,1 +1,3 @@
-mvn install -Pcollect-stats -Dpsicquic.registry.url="http://www.ebi.ac.uk/Tools/webservices/psicquic/registry/registry?action=ACTIVE&format=txt&restricted=n"
+#!/usr/bin/env bash
+#mvn install -Pcollect-stats -Dpsicquic.registry.url="http://www.ebi.ac.uk/Tools/webservices/psicquic/registry/registry?action=ACTIVE&format=txt&restricted=n"
+mvn test exec:java -Dexec.mainClass=org.hupo.psi.mi.psicquic.stats.PsicquicStatsCollector -Ppsicquic-stats
