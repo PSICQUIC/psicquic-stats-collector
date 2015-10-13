f874e017cd52fa8ca32b7076a178819c419b4acf
maximiliankoch <tschaka1904@gmail.com>
2015-10-13 14:13:09 +0100

    better mvn command

--- run-imex-collector.sh
+++ run-imex-collector.sh
@@ -1,2 +1,3 @@
-mvn -X clean install -Pcollect-stats-imex -Dpsicquic.interaction.miql.query="(interaction_id:imex)" -Dpsicquic.publication.miql.query="(pubid:imex)" -Dpsicquic.registry.url="http://www.ebi.ac.uk/Tools/webservices/psicquic/registry/registry?action=STATUS&tags=%22imex%20curation%22&format=txt"
+#!/usr/bin/env bash
+mvn test exec:java -Dexec.mainClass=org.hupo.psi.mi.psicquic.stats.PsicquicStatsCollector -Ppsicquic-stats-imex
 
